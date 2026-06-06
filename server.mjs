e4f9dc4dc3f5aab206170455d1f548a46366a493
Hasan Merchant <merchanthasan2@gmail.com>
2026-06-06 14:26:23 +0530

    Add IPN Catalyst favicon

--- server.mjs
+++ server.mjs
@@ -9,8 +9,10 @@ const host = process.env.HOST || "127.0.0.1";
 const contentTypes = {
   ".css": "text/css; charset=utf-8",
   ".html": "text/html; charset=utf-8",
+  ".ico": "image/x-icon",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".webmanifest": "application/manifest+json; charset=utf-8",
   ".png": "image/png",
   ".svg": "image/svg+xml; charset=utf-8",
   ".txt": "text/plain; charset=utf-8"
